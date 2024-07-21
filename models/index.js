e0324659dac8948dbd02c712957bdf254e86b735
BobbyValet304 <jalenwilliams90@yahoo.com>
2024-07-21 18:49:39 -0400

    established categories, products, product-tags, tags, and all routes, but haven't tested yet

--- models/index.js
+++ models/index.js
@@ -1,15 +1,46 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
+Category.belongsTo(Product, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
