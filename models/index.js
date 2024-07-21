3cc7787457d4ebff6f250c64b122aa6d513e62d8
BobbyValet304 <jalenwilliams90@yahoo.com>
2024-07-21 19:32:27 -0400

    updates index.js as to fix errors with product and category conflicts

--- models/index.js
+++ models/index.js
@@ -5,24 +5,17 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.hasOne(Category, {
+
+Product.belongsTo(Category, {
   foreignKey: "category_id",
-  onDelete: "CASCADE",
 });
 
-Category.belongsTo(Product, {
-  foreignKey: "category_id",
-});
 // Categories have many Products
-
 Category.hasMany(Product, {
   foreignKey: "category_id",
   onDelete: "CASCADE",
 });
 
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-});
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
